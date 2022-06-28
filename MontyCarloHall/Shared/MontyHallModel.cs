5573098af44b3395fc8570bc1d7ce0cf63fb4ca5
relicx74 <relicx74@gmail.com>
2022-06-27 23:25:07 -0700

    Looks like it's 50%, not 66% as some claim.  Makes sense, since at the time the 3rd (losing door) is removed, there remains 1 winning door and 1 losing door.  The initial 3 door situation no longer applies according to a basic simulation.

---
+++ MontyCarloHall/Shared/MontyHallModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MontyCarloHall.Shared
+{
+    public class MontyHallModel
+    {
+        //[StringLength(10, ErrorMessage = "Name is too long.")]
+        //public string? Name { get; set; }
+        public Boolean PlayerStrategySwitch { get; set;}
+        public Boolean PlayerStrategyStay { get; set; }
+        public Decimal WinPercent { get; set; }
+        public Decimal Wins { get; set; }
+        public Decimal Runs { get; set; }
+
+    }
+}
